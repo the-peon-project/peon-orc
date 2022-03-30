0feb5992cc780cbc4d5c23746fcfc0e60a0e1b94
Umlatt <theplacetosendmemail@gmail.com>
2022-03-30 19:12:49 +0000

    Moved client into requests

--- app/actions/servers.py
+++ app/actions/servers.py
@@ -4,18 +4,19 @@ import docker
 
 prefix = "peon.warcamp."
 
-def server_start(client,serverid):
-    logging.info("Starting server [{0}]".format(serverid))
-    container = client.containers.get("{0}{1}".format(prefix,serverid))
+def server_start(server_uid):
+    logging.info("Starting server [{0}]".format(server_uid))
+    client = docker.from_env()
+    container = client.containers.get("{0}{1}".format(prefix,server_uid))
     container.start()
 
-def server_stop(client,serverid):
-    logging.info("Stopping server [{0}]".format(serverid))
-    container = client.containers.get("{0}{1}".format(prefix,serverid))
+def server_stop(server_uid):
+    logging.info("Stopping server [{0}]".format(server_uid))
+    client = docker.from_env()
+    container = client.containers.get("{0}{1}".format(prefix,server_uid))
     container.stop()
 
 # Start flask listener
 if __name__ == "__main__":
     logging.basicConfig(filename='/var/log/peon/DEV.peon.orc_actions_servers.log', filemode='a', format='%(asctime)s %(thread)d [%(levelname)s] - %(message)s', level=logging.DEBUG)
-    client = docker.from_env()
     server_start(client,"csgo.server01")
