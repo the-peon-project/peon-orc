9326150d2178eb2e9c489cc955db0d0dcae90bc8
Umlatt <theplacetosendmemail@gmail.com>
2022-03-30 20:32:06 +0000

    Get servers from host

--- app/modules/servers.py
+++ app/modules/servers.py
@@ -4,6 +4,26 @@ import docker
 
 prefix = "peon.warcamp."
 
+def servers_reload_current():
+    client = docker.from_env()
+    containers = client.containers.list(all)
+    game_servers = []
+    servers = []
+    for game_server in containers:
+        if prefix in game_server.name:
+            game_servers.append(game_server)
+    for indx,game_server in enumerate(game_servers):
+        server_full_uid = (game_server.name).split('.')
+        server = {
+                'id' : indx + 1,
+                'game_uid' : server_full_uid[2],
+                'servername' : server_full_uid[3],
+                'password' : "**********",
+                'state' : game_server.status
+        }
+        servers.append(server)
+    return servers
+
 def server_start(server_uid):
     logging.info("Starting server [{0}]".format(server_uid))
     client = docker.from_env()
@@ -19,4 +39,4 @@ def server_stop(server_uid):
 # MAIN - for dev purposes
 if __name__ == "__main__":
     logging.basicConfig(filename='/var/log/peon/DEV.peon.orc_actions_servers.log', filemode='a', format='%(asctime)s %(thread)d [%(levelname)s] - %(message)s', level=logging.DEBUG)
-    server_start("csgo.server01")
+    print (servers_reload_current())
