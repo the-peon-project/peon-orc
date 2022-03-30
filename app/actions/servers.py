2a480f26753c0202489ce285616e7d4b4db15a2f
Umlatt <theplacetosendmemail@gmail.com>
2022-03-30 14:31:32 +0000

    Initial commit start/stop

---
+++ app/actions/servers.py
@@ -0,0 +1,20 @@
+import logging
+import docker
+
+prefix = "peon.warcamp."
+
+def server_start(client,serverid):
+    logging.info("Starting server [{0}]".format(serverid))
+    container = client.containers.get("{0}{1}".format(prefix,serverid))
+    container.start()
+
+def server_stop(client,serverid):
+    logging.info("Stopping server [{0}]".format(serverid))
+    container = client.containers.get("{0}{1}".format(prefix,serverid))
+    container.stop()
+
+# Start flask listener
+if __name__ == "__main__":
+    logging.basicConfig(filename='/var/log/peon/dev/peon-orc_actions_servers.log', filemode='a', format='%(asctime)s %(thread)d [%(levelname)s] - %(message)s', level=logging.DEBUG)
+    client = docker.from_env()
+    server_start(client,"csgo.server01")
