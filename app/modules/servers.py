5831512462da8eeef303f5d92da5ede01638f86c
Umlatt <theplacetosendmemail@gmail.com>
2022-04-27 18:18:33 +0000

    Added config input handlers

--- app/modules/servers.py
+++ app/modules/servers.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import logging
+import traceback
 import json
 from modules import client, servers, settings, prefix
 from .shell import execute_shell
@@ -46,9 +47,20 @@ def server_delete(server_uid):
     container = client.containers.get("{0}{1}".format(prefix, server_uid))
     container.remove()
 
+def add_envs(env_vars,content):
+    for key in content.keys():
+        env_vars[key] = content[key]
+    return env_vars
 
-def server_create(server_uid, user_settings={}):
+def file_json(path,content):
+    with open(path, 'w') as f: json.dump(content, f)
+
+def file_txt(path,content):
+    with open(path, 'w') as f: f.write(content)
+
+def server_create(server_uid, settings=[]):
     # START
+    error = "none"
     root_path = "/root/peon"
     server_root_path = "{0}/servers".format(root_path)
     plan_root_path = "{0}/plans".format(root_path)
@@ -58,8 +70,7 @@ def server_create(server_uid, user_settings={}):
     container_name = "{0}{1}".format(prefix, server_uid)
     logging.info("Server deplyment requested [{0}]".format(container_name))
     # STEP 1: Initialise game paths
-    execute_shell(
-        "mkdir -p {0}/data {0}/config {0}/logs && chown -R 1000:1000 {0}".format(server_path))
+    execute_shell("mkdir -p {0}/data {0}/config {0}/logs".format(server_path))
     # STEP 2: Import plan config data
     config = json.load(
         open("{0}/games/{1}/config.json".format(plan_root_path, game_uid), 'r'))
@@ -86,24 +97,45 @@ def server_create(server_uid, user_settings={}):
     container_config["volumes"]["{0}/logs".format(
         server_path)] = container_config["volumes"]["log_path"].copy()
     del container_config["volumes"]["log_path"]
-    container = client.containers.run(
-        container_config["image"],
-        name=container_name,
-        working_dir=container_config["working_directory"],
-        user=container_config["user"],
-        volumes=container_config["volumes"],
-        ports=container_config["ports"],
-        environment=container_config["variables"],
-        command=container_config["command"],
-        detach=True,
-        tty=True
-    )
-    # STEP 4: Run server scripts to deploy container
-    logging.debug("Exectuing commands in container")
-    logging.debug(" {0}".format(server_config["commands"]))
-    for shell_command in server_config["commands"]:
-        container.exec_run(
-            shell_command, user=container_config["user"], environment=container_config["variables"], detach=True, tty=True)
+    # STEP 4 - Process settings
+    for setting in settings:
+        print (setting)
+        if 'env' in setting["type"]: container_config["variables"] = add_envs(container_config["variables"],setting["content"])
+        elif 'json' in setting["type"]: file_json("{0}/config/{1}".format(server_path,setting["name"]),setting["content"])
+        elif 'txt' in setting["type"]: file_txt("{0}/config/{1}".format(server_path,setting["name"]),setting["content"])
+    # Set all file/path ownership
+    execute_shell("chown -R 1000:1000 {0}".format(server_path))
+    # STEP 5 - Start container
+    if "-REQUIRED-" in container_config["variables"]:
+        error = "Not all required settings were provided. Please confirm required settings."
+    try:
+        if error == "none":
+            container = client.containers.run(
+                container_config["image"],
+                name=container_name,
+                working_dir=container_config["working_directory"],
+                user=container_config["user"],
+                volumes=container_config["volumes"],
+                ports=container_config["ports"],
+                environment=container_config["variables"],
+                command=container_config["command"],
+                detach=True,
+                tty=True
+            )
+            # STEP 6: Run server scripts to deploy container
+            logging.debug("Executing commands in container")
+            logging.debug(" {0}".format(server_config["commands"]))
+            for shell_command in server_config["commands"]:
+                container.exec_run(
+                    shell_command, user=container_config["user"], environment=container_config["variables"], detach=True, tty=True)
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        error = "Container failed to start. Please check orc.logs for details."
+        try: 
+            container.remove()
+        except:
+            logging.warn("Failed container [{0}] was not removed.".format(container_name))
+    return error
 
 
 # MAIN - for dev purposes
