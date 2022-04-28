a557be47c7a43317f2593f7ec8c3f2ddbc914d5b
Umlatt <theplacetosendmemail@gmail.com>
2022-04-28 20:29:29 +0000

    Added plan download

--- app/modules/plans.py
+++ app/modules/plans.py
@@ -4,11 +4,12 @@ import traceback
 import json
 from pathlib import Path
 import urllib.request
-from modules import client, servers, prefix
-from .shell import execute_shell
+from shell import execute_shell
 
 root_path = "/root/peon"
-plan_file_path = "{0}/plans/plans.json".format(root_path)
+plan_path = "{0}/plans".format(root_path)
+plan_game_path = "{0}/games".format(plan_path)
+plan_file_path = "{0}/plans.json".format(plan_path)
 
 
 def get_latest_plans_list():
@@ -29,3 +30,56 @@ def plans_get_current():
         except Exception as e:
             logging.error(traceback.format_exc())
             return {"error":"Could not get latest plans."}
+
+def download_shared_plan():
+    logging.debug("Downloading shared plans".format())
+    url = "https://github.com/nox-noctua-consulting/peon-plans/archive/master.tar.gz"
+    urllib.request.urlretrieve(
+        url, "{0}/master.tar.gz".format(plan_path))
+    execute_shell('''
+    cd {0} && 
+    rm -rf {0}/shared &&
+    tar -xvf master.tar.gz &&
+    cp -R peon-plans-master/shared . &&
+    rm -rf peon-plans-master && 
+    chown 1000:1000 shared
+    '''.format(plan_path))
+
+
+def download_game_plan(url):
+    logging.info("Downloading game plan from {0}".format(url))
+    urllib.request.urlretrieve(
+        url, "{0}/master.tar.gz".format(plan_game_path))
+    main_dir = execute_shell("cd {0} && tar -tvf master.tar.gz | grep drwx".format(
+        plan_game_path))
+    main_dir = (main_dir[0].split(" "))[-1]
+    game_dir = (str(main_dir)).replace("-main/","")
+    execute_shell ('''
+        cd {0} && 
+        rm -rf {1} && 
+        tar -xvf master.tar.gz && 
+        mv {2} {1} && 
+        rm -rf master.tar.gz'''.format(plan_game_path,game_dir,main_dir)
+        )
+
+def get_plan_url(game_uid):
+    plans = plans_get_current()
+    for plan in plans:
+        if plan["game_uid"] == game_uid:
+            return plan["source"]
+        else:
+            return "NOT-FOUND"
+
+def get_plan(game_uid):
+    logging.debug("Get plan for Game_UID [{0}]".format(game_uid))
+    url = get_plan_url(game_uid)
+    if url != "NOT-FOUND":
+        download_game_plan(url)
+        return json.load(open("{0}/{1}/config.json".format(plan_game_path,game_uid)))
+    else:
+        return {"error" : "Game not found"}
+
+if __name__ == "__main__":
+    logging.basicConfig(filename='/var/log/peon/DEV.peon.orc_actions_plans.log', filemode='a',
+                        format='%(asctime)s %(thread)d [%(levelname)s] - %(message)s', level=logging.DEBUG)
+    print (get_plan('csgo'))
