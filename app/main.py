0c75018edefcb17ce00e7ffca7642cef15d434da
Umlatt <theplacetosendmemail@gmail.com>
2022-05-31 10:50:15 +0000

    Removed scheduler stuff & added devMode

--- app/main.py
+++ app/main.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
 import logging
-#from xmlrpc.client import Server
 from flask import Flask
 from flask_restful import Api
 # Import Peon Modules
 from modules.api_v1 import *
 from modules.servers import *
+from modules import *
 
 # Configure CORS (secure http/s requests)
 from flask_cors import CORS
 cors_allowed_headers=["Content-Type", "api_key", "Authorization"]
 cors_allowed_methods=["GET","POST","DELETE","PUT","PATCH","OPTIONS"]
-
-# Scheduler
-from datetime import datetime
-from apscheduler.schedulers.blocking import BlockingScheduler
-def my_job(text):
-    print(text)
 
 # Initialize Flask
 app = Flask(__name__)
@@ -30,11 +24,8 @@ api_v1.add_resource(Plans, "/api/1.0/plans")
 
 # Start flask listener
 if __name__ == "__main__":
-    sched = BlockingScheduler()
-    sched.add_job(my_job, 'date', run_date=datetime(2022, 5, 11, 19, 38, 0), args=['text'])
-    sched.start()
-
-    servers_get_all()
     logging.basicConfig(filename='/var/log/peon/orc.log', filemode='a', format='%(asctime)s %(thread)d [%(levelname)s] - %(message)s', level=logging.DEBUG)
-    logging.debug(app.run(host="0.0.0.0", port=5000, debug=False))
+    devMode()
+    servers_get_all()
+    logging.debug(app.run(host="0.0.0.0", port=5000, debug=True))
     
