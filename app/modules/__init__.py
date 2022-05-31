197cc5aea049f38851c215219ff9a96d427f1298
Umlatt <theplacetosendmemail@gmail.com>
2022-05-31 10:24:52 +0000

    Added dev switch

--- app/modules/__init__.py
+++ app/modules/__init__.py
@@ -1,12 +1,24 @@
 import json
+import os
 import docker
+import logging
+
+project_path = "/".join(((os.path.dirname(__file__)).split("/"))[:-1])
+
+# Settings file
+settings = json.load(open(f"{project_path}/settings.json", 'r'))
+# Container prefix
+prefix = "peon.warcamp."
+
+def devMode():
+    if os.path.isdir(f"{project_path}/dev"):
+        logging.info("DEV MODE ENABLED")
+        return True
+    else:
+        return False
 
 # Server list
 global servers
 servers = []
 # Docker client file
 client = docker.from_env()
-# Settings file
-settings = json.load(open("settings.json", 'r'))
-# Container prefix
-prefix = "peon.warcamp."
