556adc9bc019b0e854f1626825807e32d1c3a663
umlatt <theplacetosendmemail@gmail.com>
2023-04-16 14:32:52 +0000

    Removed auth check

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@
 import logging
 from flask import Flask
 from flask_restful import Api
-import time
 # Import Peon Modules
 from modules.api_v1 import *
 from modules.servers import *
@@ -13,8 +12,7 @@ from flask_cors import CORS
 cors_allowed_headers = ["Content-Type", "api_key", "Authorization"]
 cors_allowed_methods = ["GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"]
 
-
-# Initialize Flask
+# # Initialize Flask
 app = Flask(__name__)
 CORS(app, allow_headers=cors_allowed_headers, methods=cors_allowed_methods)
 api_v1 = Api(app)
@@ -29,17 +27,6 @@ if __name__ == "__main__":
     logging.basicConfig(filename='/var/log/peon/orc.log', filemode='a',
                         format='%(asctime)s %(thread)d [%(levelname)s] - %(message)s', level=logging.DEBUG)
     logging.debug("[START]")
-    # Check for DEV mode (contents of the /dev dir are ignored by the docker build, but can be used in development)
-    dev_mode()
-    # Verify that the orchestrator has access to the underlying host
-    authorized = "NOK"
-    while authorized != 'OK':
-        try:
-            authorized = execute_shell("ssh 172.20.0.1 -p 22222 echo 'OK'")[0]
-        except:
-            logging.error(
-                "############################## Orchestrator not authorised!!! ##############################")
-            time.sleep(5)
     # Start the schedulers timer
     scheduler_tick()
     # load all registered servers into memory
