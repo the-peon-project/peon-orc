0ce8379ac3c2367695b10facca1e1035cd0e1588
umlatt <theplacetosendmemail@gmail.com>
2023-04-23 20:13:53 +0000

    Version checker

--- app/modules/__init__.py
+++ app/modules/__init__.py
@@ -16,3 +16,25 @@ global servers
 servers = []
 # Docker client file
 client = docker.from_env()
+
+def get_newest_version(version_local, version_remote):
+    # split the version strings into their components
+    version_local_components = [int(c) for c in version_local.split('.')]
+    version_remote_components = [int(c) for c in version_remote.split('.')]
+    # compare the major version component
+    if version_local_components[0] < version_remote_components[0]:
+        return "remote"
+    elif version_local_components[0] > version_remote_components[0]:
+        return "local"
+    # compare the minor version component
+    if version_local_components[1] < version_remote_components[1]:
+        return "remote"
+    elif version_local_components[1] > version_remote_components[1]:
+        return "local"
+    # compare the patch version component
+    if version_local_components[2] < version_remote_components[2]:
+        return "remote"
+    elif version_local_components[2] > version_remote_components[2]:
+        return "local"
+    # the versions are equal
+    return "same"
