ef6f1781e717998595dfa3ba18d62c2fa032a1a4
umlatt <theplacetosendmemail@gmail.com>
2023-05-05 21:09:08 +0000

    Change server create - untested

--- app/modules/servers.py
+++ app/modules/servers.py
@@ -5,7 +5,7 @@ import json
 from pathlib import Path
 import sys
 sys.path.insert(0,'/app')
-from modules import client, servers, prefix, schedule_file
+from modules import client, prefix, schedule_file
 from modules.shell import execute_shell
 from modules.plans import *
 #from .scheduler import schedule_read_from_disk
@@ -38,7 +38,7 @@ def server_get_server(server):
         with open(f'{server_path}/config.json', 'r') as file:
             config_data = dict(json.load(file))
         description = config_data['metadata']['description']
-        version_peon = config_data['metadata']['version']
+        #version_peon = config_data['metadata']['version']
     except:
         description = "None - Please add a description"        
     try:
@@ -82,7 +82,7 @@ def server_check(server_uid):
 
 
 def servers_get_all():
-    logging.debug("Checking exisitng servers")
+    logging.debug("Checking existing servers")
     servers = []
     containers = client.containers.list(all)
     game_servers = []
@@ -135,109 +135,112 @@ def add_envs(env_vars, content):
         env_vars[key] = content[key]
     return env_vars
 
-
 def file_json(path, content):
     with open(path, 'w') as f:
         json.dump(content, f)
-
 
 def file_txt(path, content):
     with open(path, 'w') as f:
         f.write(content)
 
-
-def server_create(server_uid, description, settings=[]):
-    # START
-    error = None
-    plan_root_path = "{0}/plans".format(root_path)
-    game_uid = server_uid.split('.')[0]
-    server_name = server_uid.split('.')[1]
-    server_path = "{0}/{1}/{2}".format(server_root_path, game_uid, server_name)
-    container_name = "{0}{1}".format(prefix, server_uid)
-    logging.info("Server deplyment requested [{0}]".format(
-        container_name))  # Pull latest server files
-    # STEP 1: Initialise game paths
-    execute_shell("mkdir -p {0}/data {0}/config {0}/logs".format(server_path))
-    with open("{0}/description".format(server_path), 'w') as f:
-        f.write(description)
-    # STEP 2: Import plan config data
-    config = json.load(
-        open("{0}/games/{1}/config.json".format(plan_root_path, game_uid), 'r'))
-    logging.debug(json.dumps(config["metadata"], indent=4, sort_keys=True))
-    # STEP 3: Deploy container with game server requirements
-    container_config = config["container_config"]
-    server_config = config["server_config"]
-    logging.debug("Container Configuration")
-    logging.debug(json.dumps(container_config, indent=4, sort_keys=True))
-    # Set shared volume path with unique name in key from config, then delete old temp key
-    container_config["volumes"]["{0}/shared".format(
-        plan_root_path)] = container_config["volumes"]["shared_plan_path"].copy()
-    del container_config["volumes"]["shared_plan_path"]
-    container_config["volumes"]["{0}/games/{1}".format(
-        plan_root_path, game_uid)] = container_config["volumes"]["unique_plan_path"].copy()
-    del container_config["volumes"]["unique_plan_path"]
-    container_config["volumes"]["{0}/data".format(
-        server_path)] = container_config["volumes"]["data_path"].copy()
-    del container_config["volumes"]["data_path"]
-    if "config" in container_config["volumes"]:
-        container_config["volumes"]["{0}/config".format(
-            server_path)] = container_config["volumes"]["config"].copy()
-        del container_config["volumes"]["config"]
-    container_config["volumes"]["{0}/logs".format(
-        server_path)] = container_config["volumes"]["log_path"].copy()
-    del container_config["volumes"]["log_path"]
-    # STEP 4 - Process settings
-    # CLEAN OLD SETTINGS
-    server_config_file = f"{server_path}/config/server.config"
-    if (Path(server_config_file)).is_file():
-        with open(server_config_file, 'w') as f:
-            f.write('Services starting...')
-    # SET REQUIRED SETTINGS
-    #container_config["variables"]["PUBLIC_IP"] = execute_shell(
-    #    "dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d '\"'")[0]
-    for setting in settings:
-        if 'env' in setting["type"]:
-            container_config["variables"] = add_envs(
-                container_config["variables"], setting["content"])
-        elif 'json' in setting["type"]:
-            file_json("{0}/config/{1}".format(server_path, setting["name"]), setting["content"])
-        elif 'txt' in setting["type"]:
-            file_txt("{0}/config/{1}".format(server_path, setting["name"]), setting["content"])
-    # Set all file/path ownership
-    execute_shell("chown -R 1000:1000 {0}".format(server_path))
-    # STEP 5 - Start container
-    if "-REQUIRED-" in container_config["variables"].values():
-        return "Not all required settings were provided. Please confirm required settings."
+def server_create(config_peon,user_settings):
+    if 'success' not in (result := create_new_warcamp(config_peon=config_peon,user_settings=user_settings))['status']: return result # type: ignore
     try:
-        container = client.containers.run(
-            container_config["image"],
-            name=container_name,
-            working_dir=container_config["working_directory"],
-            user=container_config["user"],
-            volumes=container_config["volumes"],
-            ports=container_config["ports"],
-            environment=container_config["variables"],
-            command=container_config["command"],
-            detach=True,
-            tty=True
-        )
-        # STEP 6: Run server scripts to deploy container
-        logging.debug("Executing commands in container")
-        logging.debug(" {0}".format(server_config["commands"]))
-        for shell_command in server_config["commands"]:
-            container.exec_run(
-                shell_command, user=container_config["user"], environment=container_config["variables"], detach=True, tty=True)
+        execute_shell(f"cd {result['server_path']} && docker-compose up -d")
     except Exception as e:
-        logging.error(traceback.format_exc())
-        error = "Container failed to start. Please check orc.logs for details."
-        try:
-            container.remove()
-        except:
-            logging.warn(f"Failed - container [{container_name}] was not removed. {e}")
-    if error == None:
-        return {"response" : "OK"}
-    else:
-        return {"error" : error}
+        logging.error(f"[server_create] Could not start the server. <{e}>")
+        return {"status" : "error", "info" : "Unable to start the server."}
+    # # START
+    # error = None
+    # plan_root_path = "{0}/plans".format(root_path)
+    # game_uid = server_uid.split('.')[0]
+    # server_name = server_uid.split('.')[1]
+    # server_path = "{0}/{1}/{2}".format(server_root_path, game_uid, server_name)
+    # container_name = "{0}{1}".format(prefix, server_uid)
+    # logging.info("Server deplyment requested [{0}]".format(
+    #     container_name))  # Pull latest server files
+    # # STEP 1: Initialise game paths
+    # execute_shell("mkdir -p {0}/data {0}/config {0}/logs".format(server_path))
+    # with open("{0}/description".format(server_path), 'w') as f:
+    #     f.write(description)
+    # # STEP 2: Import plan config data
+    # config = json.load(
+    #     open("{0}/games/{1}/config.json".format(plan_root_path, game_uid), 'r'))
+    # logging.debug(json.dumps(config["metadata"], indent=4, sort_keys=True))
+    # # STEP 3: Deploy container with game server requirements
+    # container_config = config["container_config"]
+    # server_config = config["server_config"]
+    # logging.debug("Container Configuration")
+    # logging.debug(json.dumps(container_config, indent=4, sort_keys=True))
+    # # Set shared volume path with unique name in key from config, then delete old temp key
+    # container_config["volumes"]["{0}/shared".format(
+    #     plan_root_path)] = container_config["volumes"]["shared_plan_path"].copy()
+    # del container_config["volumes"]["shared_plan_path"]
+    # container_config["volumes"]["{0}/games/{1}".format(
+    #     plan_root_path, game_uid)] = container_config["volumes"]["unique_plan_path"].copy()
+    # del container_config["volumes"]["unique_plan_path"]
+    # container_config["volumes"]["{0}/data".format(
+    #     server_path)] = container_config["volumes"]["data_path"].copy()
+    # del container_config["volumes"]["data_path"]
+    # if "config" in container_config["volumes"]:
+    #     container_config["volumes"]["{0}/config".format(
+    #         server_path)] = container_config["volumes"]["config"].copy()
+    #     del container_config["volumes"]["config"]
+    # container_config["volumes"]["{0}/logs".format(
+    #     server_path)] = container_config["volumes"]["log_path"].copy()
+    # del container_config["volumes"]["log_path"]
+    # # STEP 4 - Process settings
+    # # CLEAN OLD SETTINGS
+    # server_config_file = f"{server_path}/config/server.config"
+    # if (Path(server_config_file)).is_file():
+    #     with open(server_config_file, 'w') as f:
+    #         f.write('Services starting...')
+    # # SET REQUIRED SETTINGS
+    # #container_config["variables"]["PUBLIC_IP"] = execute_shell(
+    # #    "dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d '\"'")[0]
+    # for setting in settings:
+    #     if 'env' in setting["type"]:
+    #         container_config["variables"] = add_envs(
+    #             container_config["variables"], setting["content"])
+    #     elif 'json' in setting["type"]:
+    #         file_json("{0}/config/{1}".format(server_path, setting["name"]), setting["content"])
+    #     elif 'txt' in setting["type"]:
+    #         file_txt("{0}/config/{1}".format(server_path, setting["name"]), setting["content"])
+    # # Set all file/path ownership
+    # execute_shell("chown -R 1000:1000 {0}".format(server_path))
+    # # STEP 5 - Start container
+    # if "-REQUIRED-" in container_config["variables"].values():
+    #     return "Not all required settings were provided. Please confirm required settings."
+    # try:
+    #     container = client.containers.run(
+    #         container_config["image"],
+    #         name=container_name,
+    #         working_dir=container_config["working_directory"],
+    #         user=container_config["user"],
+    #         volumes=container_config["volumes"],
+    #         ports=container_config["ports"],
+    #         environment=container_config["variables"],
+    #         command=container_config["command"],
+    #         detach=True,
+    #         tty=True
+    #     )
+    #     # STEP 6: Run server scripts to deploy container
+    #     logging.debug("Executing commands in container")
+    #     logging.debug(" {0}".format(server_config["commands"]))
+    #     for shell_command in server_config["commands"]:
+    #         container.exec_run(
+    #             shell_command, user=container_config["user"], environment=container_config["variables"], detach=True, tty=True)
+    # except Exception as e:
+    #     logging.error(traceback.format_exc())
+    #     error = "Container failed to start. Please check orc.logs for details."
+    #     try:
+    #         container.remove()
+    #     except:
+    #         logging.warn(f"Failed - container [{container_name}] was not removed. {e}")
+    # if error == None:
+    #     return {"response" : "OK"}
+    # else:
+    #     return {"error" : error}
 
 
 # MAIN - for dev purposes
