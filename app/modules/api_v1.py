57c91361319397c076f6c1ab1b118d2e1e6f63e8
umlatt <theplacetosendmemail@gmail.com>
2023-05-05 19:03:36 +0000

    Reworked api to not use global servers variable

--- app/modules/api_v1.py
+++ app/modules/api_v1.py
@@ -10,32 +10,12 @@ import logging
 import traceback
 import time
 
-# Schema For the Server Request JSON
-serverFields = {
-    "game_uid": fields.String,
-    "servername": fields.String,
-    "container_state": fields.String,
-    "server_state": fields.String,
-    "server_config" : fields.String,
-    "description": fields.String,
-    "time": fields.String
-}
-
-planFields = {
-    "game_uid": fields.String,
-    "title": fields.String,
-    "logo": fields.String,
-    "source": fields.String
-}
-
 class Server(Resource):
     def __init__(self):
         # Initialize The Flask Request Parser and add arguments as in an expected request
         self.reqparse = reqparse.RequestParser()
         self.reqparse.add_argument("game_uid", type=str, location="json")
         self.reqparse.add_argument("servername", type=str, location="json")
-        self.reqparse.add_argument("container_state", type=str, location="json")
-        self.reqparse.add_argument("server_state", type=str, location="json")
         self.reqparse.add_argument("description", type=str, location="json")
         self.reqparse.add_argument("eradicate", type=str, location="json")
         self.reqparse.add_argument("interval", type=str, location="json")
@@ -51,12 +31,9 @@ class Server(Resource):
                 "{0}{1}".format(prefix, server_uid)))
             if action == "stats":
                 server["stats"] = server_get_stats(server_uid)
-                serverFieldsWithStats = serverFields.copy()
-                serverFieldsWithStats["stats"] = fields.String
-                return{"server": marshal(server, serverFieldsWithStats)}, 200
-            return{"server": marshal(server, serverFields)}, 200
+            return server, 200
         except Exception as e:
-            logging.error(traceback.format_exc())
+            logging.error(f"[API-Server-GET] Failed to get server information. {e}")
             return {"error": "There was an issue getting the server."}, 404
 
     # PUT - Given an id
@@ -70,13 +47,13 @@ class Server(Resource):
         except:
             #args= {'game_uid': None, 'servername': None, 'container_state': None, 'server_state': None, 'description': None, 'eradicate': None, 'interval': None, 'epoch_time': None}
             args= {}
-            logging.debug("No arguments passed")
+            logging.debug("[API-Server-PUT] No arguments passed")
         try:
             server = server_get_server(client.containers.get(
                 "{0}{1}".format(prefix, server_uid)))
         except Exception as e:
             logging.error(traceback.format_exc())
-            return {"error": "The server [{0}] is inaccessible. Is the name valid?".format(server_uid)}, 404
+            return {"error": f"The server [{server_uid}] is inaccessible. Is the name valid? {e}" }, 404
         if action == "start":
             result = scheduler_stop_request(server_uid,args)
             if "response" in result:
@@ -99,7 +76,7 @@ class Server(Resource):
         time.sleep(0.5)
         server = server_get_server(client.containers.get("{0}{1}".format(prefix, server_uid)))
         if "response" in result:
-            return {"server": marshal(server, serverFields)}, 200
+            return server, 200
         else:
             return result, 400
 
@@ -116,10 +93,9 @@ class Server(Resource):
                     "{0}{1}".format(prefix, server_uid)))
             except Exception as e:
                 logging.error(traceback.format_exc())
-                return {"error": "The server {0} is inaccessible. Is the name valid?".format(server_uid)}, 404
+                return {"error": f"The server {server_uid} is inaccessible. Is the name valid? <{e}>"}, 404
             server_stop(server_uid)
             server_delete(server_uid)
-            servers_get_all()
             note = "Server {0} was removed."
         args = self.reqparse.parse_args()
         if "eradicate" in args and args["eradicate"] == "True":
@@ -144,8 +120,8 @@ class Servers(Resource):
     def get(self):
         logging.debug("APIv1 - Servers Get/List")
         if not authorized(request.headers): return {"error" : "Not authorized."}, 401
-        servers_get_all()
-        return{"servers": [marshal(server, serverFields) for server in servers]}
+        servers = servers_get_all()
+        return servers, 200
     # POST - Create a server
 
     def post(self):
@@ -159,15 +135,14 @@ class Servers(Resource):
             "settings": args["settings"]
         }
         try:
-            servers_get_all()
+            servers = servers_get_all()
             for serv in servers:
                 if args["game_uid"] == serv["game_uid"] and args["servername"] == serv["servername"]:
                     return{"error": "Server already exists."}, 501
             if "settings" not in args.keys():
                 args["settings"] = []
             get_latest_plans_list()
-            download_shared_plans()  # Pull latest shared files
-            response = get_plan(args["game_uid"])
+            response = get_plan_from_repo(args["game_uid"])
             if "response" in response:
                 response = server_create("{0}.{1}".format(
                     args["game_uid"], args["servername"]),
