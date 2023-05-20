3db5ef25e56c771195cf5db0faa4f5c77ff67bcd
umlatt <theplacetosendmemail@gmail.com>
2023-05-20 11:18:56 +0000

    Moved to status over response

--- app/modules/api_v1.py
+++ app/modules/api_v1.py
@@ -34,48 +34,50 @@ class Server(Resource):
             return server, 200
         except Exception as e:
             logging.error(f"[API-Server-GET] Failed to get server information. {e}")
-            return {"error": "There was an issue getting the server."}, 404
+            return {"status" : "error" , "info" : "There was an issue getting the server."}, 404
 
-    # PUT - Given an id
+    # PUT - Runs an action on a server
     def put(self, action, server_uid):
-        logging.info(
-            "APIv1 - Server {0} - Action {1}".format(server_uid, action))
-        result = { "response" : "OK"}
+        logging.info("APIv1 - Server {0} - Action {1}".format(server_uid, action))
+        result = { "status" : "success"}
         if not authorized(request.headers): return "Not authorized", 401
         try:
             args=self.reqparse.parse_args()
         except:
-            #args= {'game_uid': None, 'servername': None, 'container_state': None, 'server_state': None, 'description': None, 'eradicate': None, 'interval': None, 'epoch_time': None}
             args= {}
             logging.debug("[API-Server-PUT] No arguments passed")
-        try:
-            server = server_get_server(client.containers.get(
-                "{0}{1}".format(prefix, server_uid)))
-        except Exception as e:
-            logging.error(traceback.format_exc())
-            return {"error": f"The server [{server_uid}] is inaccessible. Is the name valid? {e}" }, 404
+        if action == "create":
+            # TODO - Implement server create
+            
+            action = "start"
+            return "Server create function is not yet implemented", 501
+        else:
+            try:
+                server = server_get_server(client.containers.get("{0}{1}".format(prefix, server_uid)))
+            except Exception as e:
+                logging.error(traceback.format_exc())
+                return {"status" : "error" , "info" : f"The server [{server_uid}] is inaccessible. Is the name valid? {e}" }, 404
         if action == "start":
             result = scheduler_stop_request(server_uid,args)
-            if "response" in result:
+            if "status" in result:
                 result = server_start(server_uid)
         elif action == "stop":
             result = scheduler_stop_request(server_uid,args)
-            if "response" in result and result["response"] == "NOW":
+            if "status" in result and result["status"] == "NOW":
                 scheduler_remove_exisiting_stop(server_uid)
                 result = server_stop(server_uid)
         elif action == "restart":
             server_restart(server_uid)
         elif action == "description":
             try:
-                server_update_description(
-                    server, (self.reqparse.parse_args())["description"])
+                server_update_description(server, (self.reqparse.parse_args())["description"])
             except:
-                return {"error": "The description argument was incorrectly provided."}, 400
+                return {"status" : "error" , "info" : "The description argument was incorrectly provided."}, 400
         else:
-            return {"error": "Unsupported action [{0}].".format(action)}, 404
+            return {"status" : "error" , "info" : "Unsupported action [{0}].".format(action)}, 404
         time.sleep(0.5)
         server = server_get_server(client.containers.get("{0}{1}".format(prefix, server_uid)))
-        if "response" in result:
+        if "status" in result:
             return server, 200
         else:
             return result, 400
@@ -93,7 +95,7 @@ class Server(Resource):
                     "{0}{1}".format(prefix, server_uid)))
             except Exception as e:
                 logging.error(traceback.format_exc())
-                return {"error": f"The server {server_uid} is inaccessible. Is the name valid? <{e}>"}, 404
+                return {"status" : "error" , "info" : f"The server {server_uid} is inaccessible. Is the name valid? <{e}>"}, 404
             server_stop(server_uid)
             server_delete(server_uid)
             note = "Server {0} was removed."
@@ -113,45 +115,12 @@ class Servers(Resource):
             "servername", type=str, required=False, help="A custom server name must be provided.", location="json")
         self.reqparse.add_argument(
             "description", type=str, required=False, help="A server description can be provided", location="json")
-    
     # GET - List all servers
     def get(self):
         logging.debug("APIv1 - Servers Get/List")
         if not authorized(request.headers): return "Not authorized", 401
         servers = servers_get_all()
         return servers, 200
-    
-    # POST - Create a server
-    def post(self):
-        logging.debug("APIv1 - Server Create")
-        if not authorized(request.headers): return "Not authorized", 401
-        if request.is_json: # Check if there is a json payload
-            settings = request.json
-            logging.error(settings)
-        # try:
-        #     servers = servers_get_all()
-        #     for serv in servers:
-        #         if args["game_uid"] == serv["game_uid"] and args["servername"] == serv["servername"]:
-        #             return{"error": "Server already exists."}, 501
-        #     if "settings" not in args.keys():
-        #         args["settings"] = []
-        #     if "response" in response:
-        #         response = server_create("{0}.{1}".format(
-        #             args["game_uid"], args["servername"]),
-        #             args["description"],
-        #             args["settings"])
-        #     if "response" in response:
-        #         servers.append(server)
-        #         time.sleep(0.5)
-        #         return server_get_server(client.containers.get(
-        #             "{0}{1}.{2}".format(prefix, args["game_uid"],args["servername"])))
-        #     else:
-        #         return response, 501
-        # except Exception as e:
-        #     logging.error(f"[API-Servers-POST] Could not create a server. <{e}>")
-        #     return {"error": "Something bad happened. Check the logs for details. Please submit to (@peon devs) to improve error handling."}, 500
-        return "Unhandled error occured", 500
-
 
 class Plans(Resource):
     def __init__(self):
