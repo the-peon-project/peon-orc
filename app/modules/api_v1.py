e63873c6d2e878b4c35fe4b10268c16157342518
umlatt <theplacetosendmemail@gmail.com>
2023-06-10 19:22:50 +0000

    Adding logging

--- app/modules/api_v1.py
+++ app/modules/api_v1.py
@@ -24,21 +24,20 @@ class Server(Resource):
 
     # GET - Returns a single server object given a matching id
     def get(self, action, server_uid):
-        logging.debug("APIv1 - Server Get - [{0}]".format(action))
+        logging.info(f"APIv1 [GET] Server <{server_uid}>")
         if not authorized(request.headers): return "Not authorized", 401
         try:
-            server = server_get_server(client.containers.get(
-                "{0}{1}".format(prefix, server_uid)))
+            server = server_get_server(client.containers.get("{0}{1}".format(prefix, server_uid)))
             if action == "stats":
                 server["stats"] = server_get_stats(server_uid)
             return server, 200
         except Exception as e:
-            logging.error(f"[API-Server-GET] Failed to get server information. {e}")
+            logging.error(f"Failed to get server information. {e}")
             return {"status" : "error" , "info" : "There was an issue getting the server."}, 404
 
     # PUT - Runs an action on a server
     def put(self, action, server_uid):
-        logging.info("APIv1 - Server {0} - Action {1}".format(server_uid, action))
+        logging.info(f"APIv1 [PUT] server <{action}> <{server_uid}>")
         result = { "status" : "success"}
         if not authorized(request.headers): return "Not authorized", 401
         server_uid_parts = server_uid.split('.')
@@ -54,30 +53,39 @@ class Server(Resource):
         self.args['server_path']=f"{settings['path']['servers']}/{self.args['game_uid']}/{self.args['warcamp']}"
         # CREATE
         if action == "create":
+            logging.debug("[create][01] Check if there are preloaded server files.")
             clean_on_fail = False
             if not os.path.isdir(self.args['server_path']): clean_on_fail = True
+            logging.debug("[create][02] Trigger [create_new_warcamp] with with settings.")
             if 'success' not in (result := create_new_warcamp(config_peon=settings,user_settings=self.args))['status']:  # type: ignore
                 if clean_on_fail:
                     shutil.rmtree(self.args['server_path'])
                 return self.args, 400
             if 'start_later' in self.args and self.args['start_later']: return result, 200
             else:
+                logging.debug("[create][03] Creation complete. Enabling server start.")
                 action = 'start'
         if action == "start":
+            logging.debug("[start][01] Check and configure server shutdown time.")
             if "response" not in (result := scheduler_stop_request(server_uid,self.args)): return result, 400 # type: ignore
+            logging.debug(f"[start][02] Trigger the startup of the server.")
             result = server_start(server_uid)
         # STOP
         elif action == "stop":
+            logging.debug("[stop][01] Check if automated shutdown is being requested server shutdown time.")
             result = scheduler_stop_request(server_uid,self.args)
             if "response" in result and result["response"] == "NOW":
+                logging.debug(f"[stop][02] Triggering server shutdown.")
                 scheduler_remove_exisiting_stop(server_uid)
                 result = server_stop(server_uid)
         # RESTART
         elif action == "restart":
+            logging.debug("[restart][01] Restarting server.")
             result = server_restart(server_uid)
         # DESCRIPTION
         elif action == "description":
             try:
+                logging.debug(f"[description][01] Updating the server description.")
                 result = server_update_description(server_uid=server_uid, description=self.args["description"])
             except:
                 return {"status" : "error" , "info" : "The description argument was incorrectly provided."}, 400
@@ -85,6 +93,7 @@ class Server(Resource):
             return {"status" : "error" , "info" : "Unsupported action [{0}].".format(action)}, 404
         time.sleep(0.5) # Wait some time for the docker daemon to do some magic
         if "status" in result:
+            logging.debug(f"Fetching the status of the server.")
             server = server_get_server(client.containers.get("{0}{1}".format(prefix, server_uid)))
             return server, 200
         else:
@@ -92,15 +101,17 @@ class Server(Resource):
 
     # DELETE - Remove a server
     def delete(self, action, server_uid):
-        logging.debug("APIv1 - Server Delete")
+        logging.info(f"APIv1 [DEL] server <{action}> <{server_uid}>")
         if not authorized(request.headers): return "Not authorized", 401
         note = ""
         if action not in ["destroy","eradicate"]:
             return {"error" : "Incorrect action [{0}] provided".format(action)}, 404
         if action == "destroy":
+            logging.debug(f"Removing the server container environment.")
             if 'success' not in ( result := server_delete(server_uid))['status']: return {"status" : "error", "info" : f"Failed to remove the server [{server_uid}].", "exception" : f"{result['exception']}"}, 404 # type: ignore
             note = "Server {0} was removed. "
         if "eradicate" in self.args and self.args["eradicate"] == "True":
+            logging.debug(f"Removing the server & user data from the filesystem.")
             server_delete_files(server_uid)
             note = note + "All files for {0} have been removed."
         return {"status" : "success", "info" : note.format(server_uid)}, 200
@@ -108,10 +119,10 @@ class Server(Resource):
 class Servers(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        
+
     # GET - List all servers
     def get(self):
-        logging.debug("APIv1 - Servers Get/List")
+        logging.info(f"APIv1 [GET] servers")
         if not authorized(request.headers): return "Not authorized", 401
         servers = servers_get_all()
         return servers, 200
@@ -128,10 +139,10 @@ class Plans(Resource):
         self.reqparse.add_argument(
             "source", type=str, required=True, help="The source files for the PEON plan must be provided.", location="json")
         self.config_peon = json.load(open("/app/config.json", 'r'))
-    # GET - List all plans
 
+    # GET - List all plans
     def get(self):
-        logging.debug("APIv1 - Plans Get/List")
+        logging.info(f"APIv1 [GET] plans")
         if not authorized(request.headers): return "Not authorized", 401
         if ( plans := get_plans_local(config_peon=self.config_peon)): # type: ignore
             return plans, 200
@@ -139,7 +150,7 @@ class Plans(Resource):
 
     # PUT - Update the plans file
     def put(self):
-        logging.debug("APIv1 - Plans update List")
+        logging.info(f"APIv1 [PUT] plans <update>")
         if not authorized(request.headers): return "Not authorized", 401
         #if ( plans := get_plans_remote(config_peon=self.config_peon)): # type: ignore
         old_plans = get_plans_local(settings)
@@ -165,7 +176,7 @@ class Plan(Resource):
         self.config_peon = json.load(open("/app/config.json", 'r'))
     
     def get(self,game_uid):
-        logging.debug("APIv1 - Plan - Get settings")
+        logging.info(f"APIv1 [GET] plans <{game_uid}>")
         if not authorized(request.headers): return "Not authorized", 401
         if ( settings := get_all_required_settings(config_peon=self.config_peon,game_uid=game_uid)): return settings, 200 # type: ignore
         else: return f"Could not get the settings for [{game_uid}]" , 404
